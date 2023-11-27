45f11530cd3e492ce57c6a598d5fd4a178b710ad
Wei Lun <weilun9320@hotmail.com>
2023-11-27 08:31:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateBMI() {
+  const firstNameInput = document.getElementById("first-name");
+  const heightInput = document.getElementById("height");
+  const weightInput = document.getElementById("weight");
+  const result = document.getElementById("result");
+
+  const firstName = firstNameInput.value;
+  const height = heightInput.value;
+  const weight = weightInput.value;
+
+  const bmi = weight / height ** 2;
+  console.log(firstName);
+  console.log(weight);
+  console.log(height);
+
+  result.innerHTML = `${firstName}, your BMI is ${bmi.toFixed(2)}!`;
+}
+
+
